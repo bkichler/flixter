b6e2ebd69ee11705cd31ba816e881f8094c9203b
Brian Kichler <brian.kichler@gmail.com>
2018-05-04 21:15:55 +0000

    Added lesson controller and show view

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -9,7 +9,7 @@ class Instructor::CoursesController < ApplicationController
     def create
         @course = current_user.courses.create(course_params)
         if @course.valid?
-            redirect_to instructor_course_path(current_course)
+            redirect_to instructor_course_path(@course)
         else
             render :new, status: :unprocessable_entity
         end
@@ -20,15 +20,15 @@ class Instructor::CoursesController < ApplicationController
 
     private
 
-    helper_method :current_course
-    def current_course
-        @current_course ||= Course.find(params[:id]) 
-    end
-
     def require_authorized_for_current_course
         if current_course.user != current_user
           return render plain: 'Unauthorized', status: :unauthorized
         end
+    end
+
+    helper_method :current_course
+    def current_course
+        @current_course ||= Course.find(params[:id]) 
     end
 
     def course_params
